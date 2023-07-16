483de29bbce59d7309e7dc7beb2d6adba240db8a
ahmednaveed19 <mahmednaveed19@gmail.com>
2023-07-16 11:52:54 -0400

    End of section 6

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
-
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500))
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
@@ -35,22 +35,18 @@ axios.interceptors.response.use(async response => {
         default:
             break;
     }
-
     return Promise.reject(error.response);
 })
-
 const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
     del: (url: string) => axios.delete(url).then(responseBody)
 }
-
 const Catalog = {
     list: () => requests.get('products'),
     details: (id: number) => requests.get(`products/${id}`)
 }
-
 const TestErrors = {
     get400Error: () => requests.get('buggy/bad-request'),
     get401Error: () => requests.get('buggy/unauthorised'),
@@ -59,9 +55,16 @@ const TestErrors = {
     getValidationError: () => requests.get('buggy/validation-error')
 }
 
+const Basket = {
+    get: () => requests.get('basket'),
+    addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+    removeItem: (productId: number, quantity = 1) => requests.del(`basket?productId=${productId}&quantity=${quantity}`)
+}
+
 const agent = {
     Catalog,
-    TestErrors
+    TestErrors,
+    Basket
 }
 
 export default agent;
---
+++ client/src/app/util/util.ts
@@ -0,0 +1,16 @@
+export function getCookie(key: string) {
+
+    const b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+  
+    return b ? b.pop() : "";
+  
+  }
+  
+  
+  
+  
+  export function currencyFormat(amount: number) {
+  
+    return '$' + (amount/100).toFixed(2);
+  
+  }
